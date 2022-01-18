fe9f30baff8f5453ee433ee04aac1098325d2e93
higorst <higor-sj@hotmail.com>
2022-01-18 12:02:17 -0300

    feat(api): finalizando requisição de abrir monitor sem aguardar retorno do comando

--- src/api/routes/monitor.py
+++ src/api/routes/monitor.py
@@ -1,7 +1,11 @@
 from flask import request
 from __main__ import app
 from codes.methods import formatResponse
-import os              
+import os             
+import threading 
+
+def command( cmd ):
+  os.system( cmd )
 
 # retornar todos os experimentos cadastrados
 @app.route('/monitor/open', methods=['GET'])
@@ -12,5 +16,6 @@ def openMonitor():
   if (mode == 1):
     input_ = "cd && rcssmonitor"
     message = 'Success - monitor ligado para a partida atual'
-  os.system(input_)
+  t1 = threading.Thread( target=command, args=(input_,) )
+  t1.start()
   return formatResponse(False, [], message=message)
