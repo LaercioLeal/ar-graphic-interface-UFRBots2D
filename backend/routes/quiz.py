0e53e56da6deaf1f9d85c7125979c6c11272e559
Higor <higor-sj@hotmail.com>
2022-05-05 21:37:36 -0300

    feat(api): rota de adição de resposta do quiz

--- backend/routes/quiz.py
+++ backend/routes/quiz.py
@@ -1,5 +1,6 @@
 from __main__ import app
-from codes.methods import formatResponse, get_db_connection
+from requests import request
+from codes.methods import formatResponse, generateHash, get_db_connection
 
 # retornar todos os experimentos cadastrados
 @app.route('/quiz/responses', methods=['GET'])
@@ -25,3 +26,35 @@ def getQuizResponses():
         }
         )
   return formatResponse(False, response)
+
+# cadastrar uma resposta de quiz
+@app.route('/quiz/responses', methods=['POST'])
+def addQuizResponse():
+    data = request.get_json()
+    id = generateHash()
+    correctQuestions = data["correctQuestions"]
+    incorrectQuestions = data["incorrectQuestions"]
+    totalScore = data["totalScore"]
+    createdAt = data["createdAt"]
+    
+
+    connection = get_db_connection()
+    cur = connection.cursor()
+    cur.execute('INSERT INTO quizResponses ('+
+        'id, '+
+        'correctQuestions, '+
+        'incorrectQuestions,'+
+        'totalScore,'+
+        'createdAt'+
+        ') VALUES (?, ?, ?, ?, ?)',
+      (
+        id, 
+        correctQuestions, 
+        incorrectQuestions,
+        totalScore,
+        createdAt
+      )
+    )
+    connection.commit()
+    connection.close()
+    return formatResponse(False, [], "Resposta adicionada")
