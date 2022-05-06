37924c3a515317dfdfc42c55fe5c71473e8652b0
Higor <higor-sj@hotmail.com>
2022-05-06 09:29:26 -0300

    feat(app): suffle array

--- src/views/Study/Quiz/components/Responding/index.js
+++ src/views/Study/Quiz/components/Responding/index.js
@@ -32,13 +32,35 @@ export default function Responding({ setResponding, setLastResult }) {
     return ids;
   };
 
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
   const getQuestions = useCallback(() => {
     const drawn = sort();
     const selectees = items.filter((item) => drawn.includes(item.id));
     setQuestions(
-      selectees.map((item) => {
-        return { ...item, selected: null };
-      })
+      shuffle(
+        selectees.map((item) => {
+          return { ...item, selected: null };
+        })
+      )
     );
   }, []);
 
