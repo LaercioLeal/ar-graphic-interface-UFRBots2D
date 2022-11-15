00c4b8d6eafe3ba6b615c214b3ab68ffc91cb294
higorst <higor-sj@hotmail.com>
2022-11-15 10:29:36 -0300

    chore(app): aviso de tutorial indisponível

--- src/views/Tutorial/index.js
+++ src/views/Tutorial/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Container, HeadingPage } from "components";
+import * as S from "./styles";
 
 import tutorialIcon from "assets/icon/tutorial.png";
 
@@ -8,6 +9,9 @@ function Tutorial() {
   return (
     <Container>
       <HeadingPage page="tutorial" title="Tutorial" icon={tutorialIcon} />
+      <div>
+        <S.Title>Ainda não disponível!</S.Title>
+      </div>
     </Container>
   );
 }
--- src/views/Tutorial/styles.js
+++ src/views/Tutorial/styles.js
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import themes from "Provider/themes";
+import { rem, transparentize } from "polished";
 import styled from "styled-components";
 
 import tutorialIcon from "assets/icon/tutorial.png";
@@ -13,4 +14,33 @@ export const Image = styled(motion.img).attrs(() => ({
   ${themes.medias.lessThan("hd")`
     height: 60px;
   `}
+`;
+
+export const Title = styled(motion.h1)`
+  transition: all 0.5s;
+  line-height: 24px;
+  font-size: ${rem(24)};
+  font-family: ${themes.fonts.bold};
+
+  color: ${themes.colors.gray};
+  color: ${transparentize(0.3, themes.colors.blue)};
+  cursor: context-menu;
+
+  ${themes.medias.lessThan("hd")`
+    font-size: ${rem(20)};
+  `}
+
+  span {
+    transition: all 0.5s;
+    text-align: center;
+    font-size: ${rem(50)};
+    color: ${transparentize(0.1, themes.colors.blue)};
+  }
+
+  :hover {
+    color: ${themes.colors.primary};
+    span {
+      color: ${themes.colors.primary};
+    }
+  }
 `;
